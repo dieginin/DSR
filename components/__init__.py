4baf5077e529440debd58d536315597e14a9b503
dieginin <diegobalestra9@gmail.com>
2024-06-10 22:00:16 -0400

    feat: create text.py

---
+++ components/__init__.py
@@ -0,0 +1,1 @@
+from .text import Subtitle, Title
---
+++ components/text.py
@@ -0,0 +1,31 @@
+import flet as ft
+
+
+class Title(ft.Text):
+    def __init__(self, value: str | None = None):
+        super().__init__()
+        self.value = value
+        self.text_align = ft.TextAlign.CENTER
+        self.style = ft.TextStyle(
+            size=60,
+            weight=ft.FontWeight.W_100,
+            letter_spacing=5,
+            foreground=ft.Paint(
+                gradient=ft.PaintLinearGradient(
+                    (0, 135), (120, 20), ["primary", "tertiary"]
+                ),
+            ),
+        )
+
+
+class Subtitle(ft.Text):
+    def __init__(self, value: str | None = None):
+        super().__init__()
+        self.value = value
+        self.text_align = ft.TextAlign.CENTER
+        self.style = ft.TextStyle(
+            size=45,
+            weight=ft.FontWeight.W_100,
+            color="secondary",
+            letter_spacing=3,
+        )
