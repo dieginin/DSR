8edb0a59fd9f99d243df28104f70b5c6e234ae65
dieginin <diegobalestra9@gmail.com>
2024-06-11 00:00:04 -0400

    update buttons.py

--- components/__init__.py
+++ components/__init__.py
@@ -1,4 +1,12 @@
-from .buttons import CustomBtn, IconBtn, PrimaryBtn, SecondaryBtn, TertiaryBtn
+from .buttons import (
+    ColorBtn,
+    CustomBtn,
+    HomeBtn,
+    IconBtn,
+    PrimaryBtn,
+    SecondaryBtn,
+    TertiaryBtn,
+)
 from .layout import CenteredColumn, CenteredRow, Section
 from .text import Subtitle, Title
 from .utils import Dialog, TextField
--- components/buttons.py
+++ components/buttons.py
@@ -1,4 +1,9 @@
+import random
+
 import flet as ft
+from flet_contrib.color_picker import ColorPicker
+
+from .utils import Dialog
 
 
 class __ElevatedButton(ft.ElevatedButton):
@@ -91,3 +96,52 @@ class IconBtn(ft.IconButton):
 
     def update_style(self):
         self.icon_color = "outlinevariant" if self.disabled else self.color
+
+
+class HomeBtn(ft.FloatingActionButton):
+    def __init__(self):
+        super().__init__()
+        self.icon = "home"
+        self.on_click = lambda e: e.page.go("/")
+
+
+class ColorBtn(ft.ElevatedButton):
+    def __init__(
+        self, value: str | None = None, show_color: bool = False, callback=None
+    ):
+        super().__init__()
+        self.value = value if value else "#%06x" % random.randint(0, 0xFFFFFF)
+        self.tooltip = self.value if show_color else "Set Color"
+        self.width = 25
+        self.height = 25
+        self.style = ft.ButtonStyle(
+            shape=ft.RoundedRectangleBorder(radius=10), bgcolor=self.value
+        )
+        self.on_click = self._open_dialog
+        self.__callback = callback
+
+    def generate_color(self) -> str:
+        self.value = "#%06x" % random.randint(0, 0xFFFFFF)
+        self.style = ft.ButtonStyle(bgcolor=self.value)
+        return self.value
+
+    def _open_dialog(self, e: ft.ControlEvent):
+        self._picker = ColorPicker(self.value)
+        self._dialog = Dialog(
+            "Choose a Color", "Choose", self._picker, on_confirm=self._update_button
+        )
+
+        e.page.dialog = self._dialog
+        self._dialog.open = True
+        e.page.update()
+
+    def _update_button(self, e: ft.ControlEvent):
+        if self.__callback:
+            if self.__callback(e, self.parent.data, self._picker.color):  # type: ignore
+                self.value = self._picker.color
+                self.bgcolor = self._picker.color
+        else:
+            self.value = self._picker.color
+            self.bgcolor = self._picker.color
+        self._dialog.open = False
+        e.page.update()
