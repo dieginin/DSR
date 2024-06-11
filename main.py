06f5547e3ffed42a0fd3421e093adc8769e14681
dieginin <diegobalestra9@gmail.com>
2024-06-10 21:36:14 -0400

    feat: create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import flet as ft
+
+from services import Router
+
+
+class Main:
+    def __init__(self, page: ft.Page):
+        super().__init__()
+        self.page: ft.Page = page
+
+        self.__init_config__()
+        self.__init_window__()
+
+    def __init_config__(self):
+        self.page.title = "SBM • Daily Sales Report"
+        self.page.theme_mode = ft.ThemeMode.DARK
+        Router(self.page)
+
+    def __init_window__(self):
+        self.page.window_height = self.page.window_min_height = 1000
+        self.page.window_width = self.page.window_min_width = 900
+        self.page.window_center()
+
+
+ft.app(Main)
