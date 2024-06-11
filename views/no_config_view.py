6949af275a923a61c6beef5d62359a529a1d8c80
dieginin <diegobalestra9@gmail.com>
2024-06-10 22:11:59 -0400

    update no_config_view.py

--- views/no_config_view.py
+++ views/no_config_view.py
@@ -1,5 +1,8 @@
 import flet as ft
 
+import components as cp
+from services import Database
+
 
 class NoConfigView(ft.View):
     def __init__(self, page: ft.Page):
@@ -11,9 +14,27 @@ class NoConfigView(ft.View):
         self.__init_config__()
 
     def __init_components__(self):
-        self.controls = []
+        self.controls = [
+            cp.Title("Need Configuration"),
+            self.subtitle(),
+            cp.SecondaryBtn("Settings", on_click=lambda _: self.page.go("/settings")),
+        ]
 
     def __init_config__(self):
         self.padding = 0
+        self.spacing = 60
         self.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-        self.vertical_alignment = ft.MainAxisAlignment.SPACE_EVENLY
+        self.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    def subtitle(self) -> cp.Subtitle:
+        store_not_setted = not self.page.client_storage.get("store_initials")
+        members_not_setted = len(Database().members) == 0
+
+        v = "You're all set."
+        if store_not_setted and members_not_setted:
+            v = "First you need to set the store and you need to add at least 1 member."
+        elif store_not_setted:
+            v = "First you need to set the store name and initials."
+        elif members_not_setted:
+            v = "First you need to add at least 1 member."
+        return cp.Subtitle(v)
